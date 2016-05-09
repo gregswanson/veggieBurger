c20e148a2a4d42bc39a2d41e632629ad744f9aa2
Gregory Swanson <gswanson@gregorys-mbp.fios-router.home>
2016-05-08 21:33:33 -0400

    res removed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,29 +9,6 @@ module.exports = function(app){
 			res.render('index', data);
 		});
 		
-
-	    //mySQL commands
-	 //    var queryString = 'SELECT * FROM burgers';
-	 //    var eatIt =[];
-	 //    var devourIt = [];
-
-
-	 //    connection.query(queryString, function(err, col) {
-	      
-	 //      if (err) throw err;
-		      
-		//       for (var i = 0; i < col.length; i++){
-		//       	if (col[i].devoured == 0){
-		//       		devourIt.push(col[i]);
-		//       	} else {
-		//       		eatIt.push(col[i]);
-		//       	}
-
-	 //        }
-	 //        var data = {eatIt: eatIt, devourIt: devourIt};
-	 //        res.render('index', data);
-	 //        //console.log(data);
-		// })
 	 })
 
 	app.put('/update', function(req,res){
@@ -39,12 +16,7 @@ module.exports = function(app){
 
 		});
 
-	    // //mySQL commands
-	    // var queryString = 'INSERT INTO burgers (burger_name, devoured) VALUES ("' + req.body.burger_type + '", 1)'; 
-	    //     connection.query(queryString, function(err, result) {
-	 
-	    //    });
-	    res.redirect('/');
+	    //res.redirect('/');
 	});
 
 	app.put('/devour/:name', function(req,res){
@@ -52,15 +24,8 @@ module.exports = function(app){
 		orm.devourBurger(burger, function(data){
 
 		});
-	    //mySQL commands
-	    
-	    //console.log(req.body.burger_type);
-	    // var queryString = 'UPDATE burgers SET devoured=0 WHERE burger_name="' + burger + '"'; 
-	    //    connection.query(queryString, function(err, result) {
-	 
-	    //    });
-	    res.redirect('/');
+	   
+	    //res.redirect('/');
 	});
 }
 
-//module.exports = burgersController;
